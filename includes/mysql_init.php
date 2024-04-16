2a9340e4d2670b2e30ed3f17748341440a093b31
Gaétan <gaetan.gerard27@gmail.com>
2024-04-16 15:29:56 +0200

    add local docker-compose + update mysql_init file

--- includes/mysql_init.php
+++ includes/mysql_init.php
@@ -8,7 +8,8 @@
 *
 */
 
-function db_connect() {
+function db_connect()
+{
     // Obtenir le chemin du répertoire parent
     $parent_dir = dirname(__DIR__);
 
@@ -19,22 +20,13 @@ function db_connect() {
     $config = parse_ini_file($config_file);
 
     // Récupérer les informations de connexion
-    $db_local_addr = $config['db_local_addr'];
-    $db_domain_addr = $config['db_domain_addr'];
+    $db_server = $config['db_server'];
     $db_user = $config['db_user'];
     $db_password = $config['db_password'];
     $db_name = $config['db_name'];
 
     // Créer une connexion à la base de données en utilisant l'adresse IP locale
-    $connection = new mysqli($db_local_addr, $db_user, $db_password, $db_name);
-
-    // Vérifier si la connexion local à réussi
-    if (!$connection) {
-        // Si la connexion locale a échoué, essayer de se connecter à la base de données à l'aide du nom de domaine
-        $connection = new mysqli($db_domain_addr, $db_user, $db_password, $db_name);
-    }
+    $connection = new mysqli($db_server, $db_user, $db_password, $db_name);
 
     return $connection;
 }
-
-
